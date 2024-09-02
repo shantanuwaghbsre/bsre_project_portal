bf8d5fee7f3e1400009aec37a05c97f2515ac437
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-09-02 16:34:46 +0530

    Update ChangePwd.tsx

--- React/bsre-front-end/src/pages/ChangePwd/ChangePwd.tsx
+++ React/bsre-front-end/src/pages/ChangePwd/ChangePwd.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Stack, TextField, Button, Typography, Box, Container } from '@mui/material';
-import { toast, ToastContainer } from "react-toastify";
 import axios from 'axios';
 import Loading from '../../Components/Loading/Loading';
 import { useRole } from "../../Contexts/RoleContext";
+import toast from 'react-hot-toast';
 
 
 
@@ -12,19 +12,19 @@ const urls = {
 };
 
 const ChangePwd = () => {
-    const [currentPassword, setCurrentPassword] = useState('');
+    // const [currentPassword, setCurrentPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [confirmNewPassword, setConfirmNewPassword] = useState('');
     const [loading, setLoading] = useState(false);
-const {username} = useRole();
+    const { username } = useRole();
     const resetForm = () => {
-        setCurrentPassword('');
+        // setCurrentPassword('');
         setNewPassword('');
         setConfirmNewPassword('');
     };
 
     const handleChangePwd = () => {
-        if (currentPassword === '' || newPassword === '' || confirmNewPassword === '') {
+        if (newPassword === '' || confirmNewPassword === '') {
             toast.error("All fields are required");
             return;
         }
@@ -38,11 +38,10 @@ const {username} = useRole();
 
         const postObject = {
             username: username,
-            current_password: currentPassword,
             new_password: newPassword,
         };
         console.log(postObject);
-        
+
 
         axios.post(urls.changePwdURL, postObject)
             .then(response => {
@@ -75,7 +74,6 @@ const {username} = useRole();
                 </div>
             ) : (
                 <>
-                    <ToastContainer style={{ width: "400px", marginTop: "60px" }} />
                     <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
                         <Box
                             sx={{
@@ -104,14 +102,7 @@ const {username} = useRole();
                                 Change Password
                             </Typography>
                             <Stack spacing={2} sx={{ width: '100%' }}>
-                                <TextField
-                                    label="Current Password"
-                                    type="password"
-                                    variant="outlined"
-                                    fullWidth
-                                    value={currentPassword}
-                                    onChange={(e) => setCurrentPassword(e.target.value)}
-                                />
+
                                 <TextField
                                     label="New Password"
                                     type="password"
