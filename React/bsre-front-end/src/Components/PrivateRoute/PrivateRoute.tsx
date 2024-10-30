b94b44eec1e8e876e3e2c0fae08606140a3811a7
shantanuwaghbsre <149784181+shantanuwaghbsre@users.noreply.github.com>
2024-10-30 13:21:08 +0530

    Update PrivateRoute.tsx

--- React/bsre-front-end/src/Components/PrivateRoute/PrivateRoute.tsx
+++ React/bsre-front-end/src/Components/PrivateRoute/PrivateRoute.tsx
@@ -3,21 +3,24 @@ import { Navigate } from "react-router-dom";
 import { useRole } from "../../Contexts/RoleContext";
 
 interface PrivateRouteProps {
-    children: React.ReactNode;
-    allowedRoles: string[];
+  children: React.ReactNode;
+  allowedRoles: string[];
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ children, allowedRoles }) => {
-    const { role, isLoggedIn } = useRole();
-    if (!isLoggedIn) {
-        return <Navigate to="/login" replace />;
-    }
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  children,
+  allowedRoles,
+}) => {
+  const { role, isLoggedIn } = useRole();
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
 
-    if (!allowedRoles.includes(role)) {
-        return <Navigate to="/not-authorized" replace />;
-    }
+  if (!allowedRoles.includes(role)) {
+    return <Navigate to="/not-authorized" replace />;
+  }
 
-    return children;
+  return children;
 };
 
 export default PrivateRoute;
